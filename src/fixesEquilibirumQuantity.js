53aa644caa31de35cc5627706f6f5e5da4ca195a
Daniel Kostro <kostro.d@gmail.com>
2016-05-13 17:15:44 +0200

    eslint fix

--- src/fixesEquilibirumQuantity.js
+++ src/fixesEquilibirumQuantity.js
@@ -1,23 +1,23 @@
 /**
  * Created by loicstrauch on 12.05.16.
  */
-"use strict";
+'use strict';
 
-module.exports={
+module.exports = {
 
-    CreateNewEquilibriumModel: function(equilibriumModel) {
-        var newEquilibriumModel=equilibriumModel;
-        newEquilibriumModel.species=[null];
-        var newComponent=newEquilibriumModel.components;
-        var newPrecipitate=newEquilibriumModel.precipitate;
+    CreateNewEquilibriumModel: function (equilibriumModel) {
+        var newEquilibriumModel = equilibriumModel;
+        newEquilibriumModel.species = [null];
+        var newComponent = newEquilibriumModel.components;
+        var newPrecipitate = newEquilibriumModel.precipitate;
         var species = equilibriumModel.species;
         var numberComponent = equilibriumModel.components.length;
         var component = equilibriumModel.components;
         var precipitate = equilibriumModel.precipitate;
-        newEquilibriumModel.constantSpecies=[];
+        newEquilibriumModel.constantSpecies = [];
         
-        var newConstant=0;
-        var numberCurrentNewSpecies=0;
+        var newConstant = 0;
+        var numberCurrentNewSpecies = 0;
 
         for (var t = 0; t < numberComponent; t++) {
             newComponent[t].species = [];
@@ -27,8 +27,8 @@ module.exports={
         for (var i = 0; i < species.length; i++) {
             if (species[i].fixesEquilibriumQuantity == false) {
 
-                newEquilibriumModel.species[numberCurrentNewSpecies]=species[i];
-                for(var u=0;u<numberComponent;u++) {
+                newEquilibriumModel.species[numberCurrentNewSpecies] = species[i];
+                for (var u = 0; u < numberComponent; u++) {
                     newComponent[u].species[numberCurrentNewSpecies] = component[u].species[i];
                 }
                 numberCurrentNewSpecies++;
@@ -37,7 +37,7 @@ module.exports={
             else {
                 for (var j = 0; j < component.length; j++) {
                     if (component[j].species[i] != 0) {
-                       newComponent[j].constant = component[j].constant * Math.pow(species[i].atEquilibrium, component[j].species[i]);
+                        newComponent[j].constant = component[j].constant * Math.pow(species[i].atEquilibrium, component[j].species[i]);
                     }
                     /*
                     for (var j = 0; j < precipitate.length; j++) {
